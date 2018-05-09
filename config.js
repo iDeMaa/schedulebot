e2ecca376e6c6a98808dfd3ca1c525881e3475ac
DeMaa <demaadota@hotmail.com>
2018-05-09 07:10:07 -0300

    Ready to deploy

--- config.js
+++ config.js
@@ -2,23 +2,23 @@ module.exports = {
 
 	// Your bot name. Typically, this is your bot's username without the discriminator.
 	// i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-	name: "ScheduleBot",
+	name: "PapitoBot",
 
 	// The bot's command prefix. The bot will recognize as command any message that begins with it.
 	// i.e: "-schedulebot foo" will trigger the command "foo",
 	//      whereas "ScheduleBot foo" will do nothing at all.
-	prefix:  "-schedulebot", // Tip: If you use "<@YOUR_BOT_USER_ID>", you can have the prefix
+	prefix:  ".papito", // Tip: If you use "<@YOUR_BOT_USER_ID>", you can have the prefix
 	                         // be a mention to your bot.
 
 	// Admin app settings
 	admin_app: {
-		desc: "ScheduleBot admin commands",
-		prefix: "schedulebot-admin"
+		desc: "PapitoBot admin commands",
+		prefix: "papitobot-admin"
 	},
 
 	// The master channel
 	// The bot will announce the events to this channel. It won't listen to other channels.
-	master_channel: "YOUR_MASTER_CHANNEL",
+	master_channel: "425861072717742089",
 
 	// Events are considered "happening" for a margin of time, where users can see that the event
 	// is happening right now. During that time, the event is not considered expired yet.+
